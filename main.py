48387555fafdf95c59b19d615011100a57d7da30
Richo-Dev <81697193+AdiRaihan46@users.noreply.github.com>
2021-09-01 23:06:12 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#ddos attack tools
+#code attackddos
+#prank ddos attack
+#python script
+#bisa di ubah sesuai selera
+ 
+number = 0
+while number < 101:
+     a = input ("Ddos Attack Server :")
+     if a == "jgrp": #bisa diganti nama
+            for i in range (0,101,1):
+              print ("Attack The Progress...",i,"%")
+              if i == 100:
+                    print ("Attack Successful...",i,"%")
+                    print ("Status Server: Down")
+                    break 
